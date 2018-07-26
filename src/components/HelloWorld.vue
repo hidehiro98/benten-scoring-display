f4587b45e8faf889f89bb5d714c9e0c39e727cf4
Hidehiro Nagaoka <kuinige@gmail.com>
2018-07-26 14:28:25 +0900

    Initial commit with helloworld

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="message">メッセージ: {{ msg }}</p>
+</template>
+
+<style>
+.message { color: #42b983; }
+</style>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: 'こんにちは！'
+    }
+  }
+}
+</script>
